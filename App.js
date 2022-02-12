924bd0372eb0529e62a2392eaa8097a963ad745a
krishiv95 <krishivbhayani@gmail.com>
2022-02-12 16:02:23 +0530

    this is my imp file

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {View,StyleSheet,Text} from "react-native";
+
+
+
+const App=()=>{
+
+ return (
+  <View style={{flex:1,backgroundColor:'black',}}>
+  </View>
+
+ );
+
+
+
+}
+export default App;
+
+ 
